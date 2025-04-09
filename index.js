857798a53c2b08f43c3b0f39c435aeaad3dc1d0b
Nguyen Thien Hung <ngthienhung277@users.noreply.github.com>
2025-04-09 10:27:15 +0700

    update first

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.use("/users", usersRoutes);
-app.get("/", (req, res) => res.send("Welcome to the User API!"));
-app.all("*", (req, res) => {
-  "You've tried reaching a route that doesn't exist!";
-});
+app.use("/people", usersRoutes);
+app.get("/", (req, res) => res.send("Welcome to the Users API!"));
+app.all("*", (req, res) => res.send("You've tried reaching a route that doesn't exist."));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
